b1112c782b24184f88ce42b9be23dfaaee28bc31
RicardoHernandezVarela <rhvarela011192@gmail.com>
2020-01-03 21:13:11 -0600

    Barra de navegación agregada

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route, 
+    Switch
+} from 'react-router-dom';
+
+/* IMPORT PATHS/ROUTES */
+import * as ROUTES from '../../constants/routes';
+
+/* IMPORT COMPONENTS */
+import Navigation from '../Navigation/Navigation'
+import LandingPage from '../Landing/LandingPage';
 
 const App = () => {
     return (
-        <div>
-            <h1>Desde /App/index.js</h1>
-        </div>
+        <Router>
+            <Navigation />
+            <hr />
+
+            <Switch>
+                <Route exact path={ROUTES.LANDING} component={LandingPage}/>
+            </Switch>
+        </Router>
     );
 }
 
---
+++ src/components/Landing/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div>
+            <h6>Landing...</h6>
+        </div>
+    );
+}
+
+export default LandingPage;
