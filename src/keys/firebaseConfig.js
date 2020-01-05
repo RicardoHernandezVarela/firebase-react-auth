110f2ee0fa8f6c836a8d55a267f907e0ac03f8bf
RicardoHernandezVarela <rhvarela011192@gmail.com>
2020-01-05 00:57:59 -0600

    Change Firebase config keys

--- src/keys/firebaseConfig.js
+++ src/keys/firebaseConfig.js
@@ -1,9 +1,9 @@
 /* Place here the firebase project config keys */
 
-export const apiKey = "AIzaSyBHsMyzmOpT4W8LwnTcuNjW9n_2XkyNBlk";
-export const authDomain =  "cake-factory-86343.firebaseapp.com";
-export const databaseURL = "https://cake-factory-86343.firebaseio.com";
-export const projectId = "cake-factory-86343";
-export const storageBucket = "cake-factory-86343.appspot.com";
-export const messagingSenderId = "858577807933";
-export const appId = "1:858577807933:web:08c8c26ceca60e36b21854";
+export const apiKey = "";
+export const authDomain =  "";
+export const databaseURL = "";
+export const projectId = "";
+export const storageBucket = "";
+export const messagingSenderId = "";
+export const appId = "";
