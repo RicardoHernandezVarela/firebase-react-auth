ffbe746c8f06422c18b18e1e3b80ffed75916bf6
RicardoHernandezVarela <rhvarela011192@gmail.com>
2020-01-05 00:44:32 -0600

    Export Firebase config keys

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,13 +1,16 @@
 import app from 'firebase/app';
 
+/* Import Firebase keys */
+import * as KEYS from '../../constants/firebaseConfig';
+
 const config = {
-    apiKey: "AIzaSyBHsMyzmOpT4W8LwnTcuNjW9n_2XkyNBlk",
-    authDomain: "cake-factory-86343.firebaseapp.com",
-    databaseURL: "https://cake-factory-86343.firebaseio.com",
-    projectId: "cake-factory-86343",
-    storageBucket: "cake-factory-86343.appspot.com",
-    messagingSenderId: "858577807933",
-    appId: "1:858577807933:web:08c8c26ceca60e36b21854"
+    apiKey: KEYS.apiKey,
+    authDomain: KEYS.authDomain,
+    databaseURL: KEYS.databaseURL,
+    projectId: KEYS.projectId,
+    storageBucket: KEYS.storageBucket,
+    messagingSenderId: KEYS.messagingSenderId,
+    appId: KEYS.appId
 };
 
 class Firebase {
