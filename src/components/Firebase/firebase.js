5878b0045faacf43b138e796e55995aef35fa395
RicardoHernandezVarela <rhvarela011192@gmail.com>
2020-01-06 15:15:21 -0600

    Auth configuration with Firebase API

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 /* Import Firebase keys */
 import * as KEYS from '../../keys/firebaseConfig';
@@ -16,7 +17,30 @@ const config = {
 class Firebase {
     constructor(){
         app.initializeApp(config);
+
+        this.auth = app.auth();
     }
+
+    /* Auth API */
+
+    /* Create new user with email and password */
+    doCreateUserWithEmailAndPassword = (email, password) => 
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    /* Sign in with email and password */
+    doSignInWithEmailAndPassword = (email, password) => 
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    /* Sign out */
+    doSignOut = () => this.auth.signOut();
+
+    /* Send email to reset password */
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+    
+    /* Update password of current user */
+    doPasswordUpdate = password => 
+        this.auth.currentUser.updatePassword(password);
+    
 }
 
 export default Firebase;
