9e17bd22d45fc21e1f8ca0dba04fd9f11dd4901c
RicardoHernandezVarela <rhvarela011192@gmail.com>
2020-01-06 20:21:36 -0600

    Session handling with firebase auth API

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     BrowserRouter as Router,
     Route, 
@@ -8,6 +8,9 @@ import {
 /* IMPORT PATHS/ROUTES */
 import * as ROUTES from '../../constants/routes';
 
+/* Import FirebaseContext */
+import { withFirebase } from '../Firebase/index';
+
 /* IMPORT COMPONENTS */
 import Navigation from '../Navigation/Navigation'
 import LandingPage from '../Landing/LandingPage';
@@ -15,20 +18,49 @@ import SignUp from '../SignUp/SignUp';
 import Home from '../Home/Home';
 import SignIn from '../SignIn/SignIn';
 
-const App = () => {
-    return (
-        <Router>
-            <Navigation />
-            <hr />
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-            <Switch>
-                <Route exact path={ROUTES.LANDING} component={LandingPage}/>
-                <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
-                <Route exact path={ROUTES.HOME} component={Home} />
-                <Route exact path={ROUTES.SIGN_IN} component={SignIn} />
-            </Switch>
-        </Router>
-    );
+        /* Session handling */
+        this.state = {
+            authUser: null,
+        };
+    }
+
+    componentDidMount() {
+        /********************************************************
+          Listener to get the authenticated user from Firebase is 
+          called when user signs up, signs in and signs out.
+        ********************************************************/
+        this.listener = this.props.firebase.auth.onAuthStateChanged(
+            authUser => {
+                authUser
+                    ? this.setState({authUser: authUser})
+                    : this.setState({authUser: null}); 
+        });
+    }
+
+    componentWillUnmount() {
+        this.listener();
+    }
+
+    render() {
+        return (
+            <Router>
+                <Navigation authUser={this.state.authUser}/>
+                <hr />
+    
+                <Switch>
+                    <Route exact path={ROUTES.LANDING} component={LandingPage}/>
+                    <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
+                    <Route exact path={ROUTES.HOME} component={Home} />
+                    <Route exact path={ROUTES.SIGN_IN} component={SignIn} />
+                </Switch>
+            </Router>
+        );
+    }
+
 }
 
-export default App;
+export default withFirebase(App);
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,34 +7,47 @@ import * as ROUTES from '../../constants/routes';
 /* Import components */
 import SignOutButton from '../SignOut/SignOut';
 
-const Navigation = () => {
+const Navigation = ({ authUser }) => {
     return (
         <div>
-            <ul>
-                <li>
-                    <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.LANDING}>Landing</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.HOME}>Home</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.ACCOUNT}>Account</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.ADMIN}>Admin</Link>
-                </li>
-                <li>
-                    <SignOutButton />
-                </li>
-            </ul>
+            {authUser ? <NavigationAuth />: <NavigationNonAuth />}
         </div>
     );
 } 
+
+const NavigationAuth = () => {
+    return (
+        <ul>
+            <li>
+                <Link to={ROUTES.LANDING}>Landing</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.HOME}>Home</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.ACCOUNT}>Account</Link>
+            </li>
+            <li>
+                <SignOutButton />
+            </li>
+        </ul>
+    );
+};
+
+const NavigationNonAuth = () => {
+    return (
+        <ul>
+            <li>
+                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.LANDING}>Landing</Link>
+            </li>
+        </ul>
+    );
+};
 
 export default Navigation;
